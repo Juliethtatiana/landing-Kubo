71bcc8afa9ff9c0ad9cdf73c277f89352a8ca187
Julieth Riano <jriano@kubo.co>
2023-11-28 09:10:03 -0500

    [update] landing kubo advances

--- components/forms/ContactForm.vue
+++ components/forms/ContactForm.vue
@@ -6,32 +6,38 @@ const countries = ref([
 ])
 </script>
 <template>
-  <div class="contact-form">
-    <div class="flex flex-column gap-2 general-input">
-      <label for="name">Nombre <span>*</span></label>
-      <InputText id="name" v-model="name" aria-describedby="name-help" />
-    </div>
-    <div class="flex flex-column gap-2 general-input">
-      <label for="name">Apellidos <span>*</span></label>
-      <InputText id="name" v-model="name" aria-describedby="name-help" />
-    </div>
-    <div class="flex gap-5">
-      <div>
-        <label for="country">País <span>*</span></label>
-        <Dropdown
-          v-model="country"
-          :options="countries"
-          optionLabel="name"
-          placeholder="País "
-          class="w-full md:w-14rem"
-        />
+  <div class="tw-flex tw-gap-40">
+    <slot name="information" />
+
+    <div class="w-full">
+      <div class="contact-form">
+        <div class="flex flex-column gap-2 general-input">
+          <label for="name">Nombre <span>*</span></label>
+          <InputText id="name" v-model="name" aria-describedby="name-help" />
+        </div>
+        <div class="flex flex-column gap-2 general-input">
+          <label for="name">Apellidos <span>*</span></label>
+          <InputText id="name" v-model="name" aria-describedby="name-help" />
+        </div>
+        <div class="flex gap-5">
+          <div>
+            <label for="country">País <span>*</span></label>
+            <Dropdown
+              v-model="country"
+              :options="countries"
+              optionLabel="name"
+              placeholder="País "
+              class="w-full md:w-14rem"
+            />
+          </div>
+          <div class="flex flex-column gap-2 general-input">
+            <label for="name">Número de Teléfono <span>*</span></label>
+            <InputText id="phone" v-model="phone" />
+          </div>
+        </div>
+        <recaptcha />
+        <Button type="submit" class="btn">Enviar</Button>
       </div>
-      <div class="flex flex-column gap-2 general-input">
-        <label for="name">Número de Teléfono <span>*</span></label>
-        <InputText id="phone" v-model="phone" />
-      </div>
     </div>
-    <recaptcha />
-    <Button type="submit" class="btn">Enviar</Button>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -20,56 +20,78 @@ const imgBrands = ref([
 </script>
 <template>
   <div class="landing">
-    <div class="tw-flex tw-gap-40">
-      <div class="contact-brands">
-        <h2>
-          Da el primer paso para crear tu app,
-          <span class="blue">Ellos ya lo hicieron:</span>
-        </h2>
-        <div class="brand">
-          <img
-            v-for="(img, index) in imgBrands"
-            :key="index"
-            :src="'/img/' + img"
-            alt=""
-          />
+    <FormsContactForm>
+      <template #information>
+        <div class="contact-brands">
+          <h2>
+            Da el primer paso para crear tu app,
+            <span class="blue">Ellos ya lo hicieron:</span>
+          </h2>
+          <div class="brand">
+            <img
+              v-for="(img, index) in imgBrands"
+              :key="index"
+              :src="'/img/' + img"
+              alt=""
+            />
+          </div>
         </div>
-      </div>
-      <div class="w-full">
-        <FormsContactForm></FormsContactForm>
-      </div>
-    </div>
+      </template>
+    </FormsContactForm>
     <div class="w-full text-center about">
-      <h2>
-          Da el primer paso para crear tu app,
-        </h2>
-        <h2 class="blue">¿Qué ofrecemos?</h2>
-<div class="tw-flex tw-flex-col tw-items-center tw-w-full tw-gap-[104px] tw-mt-[104px]">
-  <div class="service-card">
-        <img src="/img/Celular_servicios.png" alt="">
-        <div>
+      <h2>Da el primer paso para crear tu app,</h2>
+      <h2 class="blue">¿Qué ofrecemos?</h2>
+      <div
+        class="tw-flex tw-flex-col tw-items-center tw-w-full tw-gap-[104px] tw-mt-[104px]"
+      >
+        <div class="service-card">
+          <img src="/img/Celular_servicios.png" alt="" />
+          <div>
             <h3>Desarrollo de apps multiplataforma y nativas</h3>
-            <p>Desarrollamos aplicaciones móviles que generan <span>ventajas competitivas en el mercado</span></p>
+            <p>
+              Desarrollamos aplicaciones móviles que generan
+              <span>ventajas competitivas en el mercado</span>
+            </p>
+          </div>
         </div>
-        
-    </div>
-    <div class=" service-card">
-        <div>
+        <div class="service-card">
+          <div>
             <h3>Desarrollo de apps multiplataforma y nativas</h3>
-            <p>Desarrollamos aplicaciones móviles que generan <span>ventajas competitivas en el mercado</span></p>
+            <p>
+              Desarrollamos aplicaciones móviles que generan
+              <span>ventajas competitivas en el mercado</span>
+            </p>
+          </div>
+          <img src="/img/Lapiz_servicios.png" alt="" />
         </div>
-        <img src="/img/Celular_servicios.png" alt="">
-    </div>
-    <div class="service-card">
-        <img src="/img/Celular_servicios.png" alt="">
-        <div>
+        <div class="service-card">
+          <img src="/img/Lupa_servicios.png" alt="" />
+          <div>
             <h3>Desarrollo de apps multiplataforma y nativas</h3>
-            <p>Desarrollamos aplicaciones móviles que generan <span>ventajas competitivas en el mercado</span></p>
+            <p>
+              Desarrollamos aplicaciones móviles que generan
+              <span>ventajas competitivas en el mercado</span>
+            </p>
+          </div>
         </div>
-        
+      </div>
     </div>
-</div>
-     
+    <div class="w-full text-center expert">
+      <h2 class="blue tw-mb-[100px]">¿Por qué escoger Kubo?</h2>
+      <div class="flex w-full justify-content-center tw-gap-[136px]">
+        <div class="expert-item">
+          <img src="/img/Gorro_expertos.png" alt="" />
+          <p>Más de 15 años de experiencia</p>
+        </div>
+        <div class="expert-item">
+          <img src="/img/Medalla_expertos.png" alt="" />
+          <p>150 proyectos realizados</p>
+        </div>
+        <div class="expert-item">
+          <img src="/img/Moneda_expertos.png" alt="" />
+          <p>Modelos de pago especiales</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
